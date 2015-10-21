133a988a10179d6918f571ed7343eb38f7d9746f
Gilles Fabio <gilles@gillesfabio.com>
2015-10-21 17:30:27 +0200

    Refactor store.

--- stores.go
+++ stores.go
@@ -1,6 +1,6 @@
 package paging
 
-import "github.com/ulule/gorm"
+import "github.com/jinzhu/gorm"
 
 // -----------------------------------------------------------------------------
 // Interfaces
@@ -8,7 +8,7 @@ import "github.com/ulule/gorm"
 
 // Store is a store.
 type Store interface {
-	Paginate(limit, offset, count int64) error
+	Paginate(limit, offset int64, count *int64) error
 }
 
 // -----------------------------------------------------------------------------
@@ -19,25 +19,29 @@ type Store interface {
 type GORMStore struct {
 	db    *gorm.DB
 	items interface{}
+	order string
 }
 
 // NewGORMStore returns a new GORM store instance.
-func NewGORMStore(db *gorm.DB, items interface{}) (*GORMStore, error) {
+func NewGORMStore(db *gorm.DB, items interface{}, order string) (*GORMStore, error) {
 	return &GORMStore{
 		db:    db,
 		items: items,
+		order: order,
 	}, nil
 }
 
 // Paginate paginates items from the store and update page instance.
-func (s *GORMStore) Paginate(limit, offset, count int64) error {
-	q := s.db.Limit(limit)
+func (s *GORMStore) Paginate(limit, offset int64, count *int64) error {
+	q := s.db
+	q = q.Limit(limit)
 	q = q.Offset(offset)
+	q = q.Order(s.order)
 	q = q.Find(s.items)
 	q = q.Limit(-1)
 	q = q.Offset(-1)
 
-	if err := q.Count(&count).Error; err != nil {
+	if err := q.Count(count).Error; err != nil {
 		return err
 	}
 
